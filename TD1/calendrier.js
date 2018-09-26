4666fa349ddb1c31021c08fffa0a0cec8aa7ceb5
antoine lamontagne <antoine.lamontagne@hotmail.ca>
2018-09-26 14:43:44 -0400

    add element de la question 2

--- TD1/calendrier.js
+++ TD1/calendrier.js
@@ -1,1 +1,11 @@
-﻿
+﻿function check(value){
+    document.getElementById("check" + value).innerHTML = `<img src="Images/tick-check.png" alt="check box" onclick="uncheck(${value})"/>`;
+    document.getElementById("nb" + value).style.background = "#ebf7d4";
+    document.getElementById("dat" + value).style.background = "#ebf7d4";
+};
+
+function uncheck(value){
+    document.getElementById("check" + value).innerHTML = `<img src="Images/check.png" alt="check box" onclick="check(${value})"/>`;
+    document.getElementById("nb" + value).style.background = "white";
+    document.getElementById("dat" + value).style.background = "white";
+};
