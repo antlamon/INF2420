762a0304b477ea35a34f7fb612870e2a64dfdb96
antoine lamontagne <antoine.lamontagne@hotmail.ca>
2018-10-03 11:50:10 -0400

    popup done

--- TD1/calendrier.js
+++ TD1/calendrier.js
@@ -15,19 +15,48 @@ function uncheck(img){
 };
 
 let timeout;
-function handlePopper(event){
-    console.log(event);
+function handlePopup(event){
     if (event.type == "mouseover"){
-        timeout = setTimeout(showPopper, 1000, event);
+        timeout = setTimeout(showPopup, 1000, event);
     }
     else{
         clearTimeout(timeout);
+        hidePopup(event);
     }
 };
 
-function showPopper(event){
-    console.log("ShowingPopper")
-    const id = event.path[event.path.length - 6].id;
-    //document.getElementById(id).innerHTML += '<span class="popuptext" id="myPopup">A Simple Popup!</span>';
-    console.log(id);
+function showPopup(event){
+    const id = event.srcElement.id;
+    let popup = `<div id="popup"><div class="popup-date-time"><div class="popup-date">`;
+    let dateTime = document.getElementById("dat" + id[7]);
+    // Get the month
+    popup += dateTime.childNodes[1].outerHTML;
+    // Get the date
+    popup += dateTime.childNodes[3].outerHTML;
+    // Get the day
+    popup += dateTime.childNodes[5].outerHTML;
+    popup += `</div><div class="popup-time">`;
+    // Get the time
+    popup += dateTime.childNodes[7].innerHTML;
+    popup += `</div></div><div class="popup-vote"><span class="popup-voter">`;
+    // Get voter name
+    popup += document.getElementById("name" + id[6]).innerHTML;
+    popup += `</span><span class="popup-voter-option">`;
+    // Get vote option
+    if (event.srcElement.className == "ticked-box"){
+        popup += `Voted "Yes"`
+    }
+    else{
+        popup += `Didn't vote for this`
+    }
+    popup += `</span></div>`;
+    document.getElementById(id).innerHTML += popup;
+}
+
+function hidePopup(event){
+    let popup = document.getElementById("popup");
+    if (popup != null){
+        let parent = popup.parentNode;
+        parent.removeChild(popup);
+    }
 }
