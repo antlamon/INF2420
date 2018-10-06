32c69ac2b98557c244703696f14aa7a5900c9468
Antoine Lamontagne <antoine.lamontagne@hotmail.ca>
2018-10-06 02:08:03 -0400

    elever les svg not ticked et center le sondage

--- TD1/calendrier.js
+++ TD1/calendrier.js
@@ -158,9 +158,7 @@ DoodleController.prototype.changeCurrentParticipant = function changeCurrentPart
                                     </div>`;
                 }
                 else {
-                   return row + `   <div id="choice-${index}-${indexDate}" class="box not-ticked-box">
-                                        <svg aria-hidden="true" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
-                                    </div>`;
+                   return row + `   <div id="choice-${index}-${indexDate}" class="box not-ticked-box"></div>`;
                 }
            },   `<div class="participant">
                     <img class="avatar" src="Images/particip2.png" alt="grey avatar" /><span class="nom-participant" id="name-${index}">${participant.name}</span>
