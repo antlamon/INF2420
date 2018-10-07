6c9706159910f73653516c5caddad0018133354d
Antoine Lamontagne <antoine.lamontagne@hotmail.ca>
2018-10-07 13:29:19 -0400

    check a coté du bouton

--- TD1/calendrier.js
+++ TD1/calendrier.js
@@ -206,7 +206,14 @@ DoodleController.prototype.updateCurrentParticipant = function updateCurrentPart
     buffer += `</div>`;
     if(doodleModel.currentParticipant) {
         var buttonValue = IsCurrentParticipantInParticipants(doodleModel)? "Annuler":"Mettre à jour";
-        buffer += `<button class="finish-button">${buttonValue}</button>`
+        var nbCheck = doodleModel.currentParticipant.disponibility.filter(Boolean).length;
+        buffer += `<div class="finish-button">`;
+        if(nbCheck > 0) {
+            buffer += ` <svg aria-hidden="true" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+                        <span>${nbCheck}</span>`;
+        }
+        buffer += `     <button class="finish-button">${buttonValue}</button>
+                    </div>`;
     }
     this.element.innerHTML = buffer;
     var boxes = this.element.querySelectorAll(".box");
