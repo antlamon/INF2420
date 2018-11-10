389c5a38e67c7088ebd813dcad299774d7a8329f
antoinelabo <antoinelabo@videotron.ca>
2018-11-10 14:31:17 -0500

    started from the bottom now where at still the bottom

---
+++ TD2/main.js
@@ -0,0 +1,6 @@
+//EntryPoint
+(async function() {
+    console.log(wtf);
+    var wtf = "string";
+    //this.connectionHandler = new ConnectionHandler(`http://log2420-nginx.info.polymtl.ca`,)
+ })();
