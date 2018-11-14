36f1b1737149f4dbc3a30bd927a3a5dd6776cf39
Antoine Lamontagne <antoine.lamontagne@hotmail.ca>
2018-11-14 15:46:04 -0500

    commencement de l'interface

--- TD2/main.js
+++ TD2/main.js
@@ -1,4 +1,12 @@
 //EntryPoint
 (async function() {
-    this.connectionHandler = new ConnectionHandler(`ws://log2420-nginx.info.polymtl.ca/`, "VarCestLet")
+    this.connectionHandler = new ConnectionHandler(`ws://log2420-nginx.info.polymtl.ca/`, "VarCestLet");
+    //var view = new PolyChatView(null)
  })();
+
+ class PolyChatView {
+     constructor() {
+         this.username = null;
+         this.currentGroup = null;
+     }
+ }
