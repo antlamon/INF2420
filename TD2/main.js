ec0232a7fed70a855087222612adb9a43df19548
antoine lamontagne <antoine.lamontagne@hotmail.ca>
2018-11-21 13:48:35 -0500

    UI global

--- TD2/main.js
+++ TD2/main.js
@@ -1,14 +1,13 @@
-
  class PolyChatView {
      constructor() {
          this.usernameContainer = document.getElementById(`user`);
-         this.currentUsername = `hello`; // à mettre dans le controller
+         this.currentUsername = `Guest`; // à mettre dans le controller
          this.chatContainer = document.getElementById(`chat-container`);
          this.chatHeader = null;
          this.chat = null;
          this.groupContainer = document.getElementById(`group-container`);
          this.groups = null;
-         this.currentGroup = `Général`;
+         this.currentGroup = "null";
         }
         
         renderView() {
@@ -24,16 +23,41 @@
             // chat header
             buffer +=   `<div class="container-header">
                             <div class="small-info-text">Groupe actif:</div>
-                            <div class="info-text">${this.currentGroup}</div>
+                            <div class="info-text">${this.currentGroup != null ? this.currentGroup : "Veuillez selectionner un groupe"}</div>
                         </div>`;
-                        
+            if (this.currentGroup != null) {
+            // conversation container
+            buffer +=   `<div id="conversation"></div>`
+
+            // message input container
+            buffer +=   `<div class="input-container">
+                            <i class="far fa-thumbs-up" id="like-button"></i>
+                            <input id="message-input" name="message-input type="text" placeholder="Votre message ici"/>
+                            <div class="small-info-text" id="send-button">Envoyer</div>
+                        </div>`
+            }
+                            
             this.chatContainer.innerHTML = buffer;
             this.refreshConversation();
         }
         
         // Render the elements in the group container
         renderGroups() {
-            
+            var buffer = ``;
+            // group header
+            buffer +=   `<div class="container-header">
+                            <div class="small-info-text">Liste des groupes:</div>
+                            <div class="available-group-header">
+                                <div class="info-text">Groupes Disponibles</div>
+                                <div class="group-options">
+                                    <i class="fa fa-plus" id="add-group"></i>
+                                    <i class="fa fa-cog" id="group-options"></i>
+                                </div>
+                            </div>
+                        </div>`;
+                        
+            // group list
+            this.groupContainer.innerHTML = buffer;
         }
         
         changeUserName() {
